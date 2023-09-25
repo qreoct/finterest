f53d025f5dda7c9c8954c17fed3d67f0eb876717
Kleon Ang <kleonang@gmail.com>
2023-09-25 19:37:29 +0800

    Fix user history updates in new ArticleFullDisplay

--- components/Article/ArticleFullDisplay.tsx
+++ components/Article/ArticleFullDisplay.tsx
@@ -1,11 +1,12 @@
 import { ArticleType } from "@/types/ArticleTypes"
 import { useEffect, useState } from "react";
-import { getArticle } from "@/config/firestore";
+import { getArticle, updateUserHistory } from "@/config/firestore";
 import { convertToArticleType } from "@/types/ArticleTypes";
 import Head from 'next/head';
 import { BiArrowBack, BiSolidMagicWand } from "react-icons/bi";
 import ProtectedRoute from '@/components/ProtectedRoute';
 import { useRouter } from 'next/router';
+import { useAuth } from '@/context/AuthContext';
 import ArticleConvo from "../ChatStuff/ArticleConvo";
 import { HighlightMenu, MenuButton } from "react-highlight-menu";
 import LeftNavigationBar  from '@/components/common/LeftNavigationBar'
@@ -16,8 +17,8 @@ export const ArticleFullDisplay = ({ articleId }: { articleId: string }) => {
     /* Routing */
     const router = useRouter();
 
-
     /* State variables */
+    const { user } = useAuth();
 
     //To pass highlighted text information to article convo, which is a child component
     let [highlightedText, setHighlightedText] = useState('');
@@ -72,6 +73,8 @@ export const ArticleFullDisplay = ({ articleId }: { articleId: string }) => {
         </div>;
     }
 
+    // Update user history on clicking into article
+    updateUserHistory(user.uid, currArticle.article_id);
 
     /* Helper Functions */
 
