2f847930b88ad9cb158e3e3256f0e1f7a1527379
Jefferson <9080974+qreoct@users.noreply.github.com>
2023-09-27 23:29:56 +0800

    feat: improve card rendering

--- components/Article/TopArticleListItem.tsx
+++ components/Article/TopArticleListItem.tsx
@@ -28,14 +28,16 @@ export const TopArticleListItem = ({ article }: { article: DocumentData }) => {
     }, [currArticle]);
 
     return (
-        <div className={`w-1/5 flex-shrink-0 pb-4`}>
+        <div className={`flex-shrink-0 pb-4`}>
             <Link href="/articles/[id]" as={`/articles/${currArticle.article_id}`}
-                className={`text-xl font-extra-bold text-stone-900 flex flex-col rounded-lg items-start justify-center space-y-2 bg-${bgColor}`}>
+                className={`text-xl font-extra-bold text-stone-900 flex flex-col max-h-100 max-w-xs
+                rounded-lg items-start justify-center space-y-2 bg-${bgColor}`}
+                style={{ minWidth: '220px', maxHeight: '520px', minHeight: '520px' }}>
                 <img src={currArticle.image_url} alt={currArticle.title} className='rounded-lg w-full h-1/16' />
                 <div className="content px-4 py-2">
                     <h5 className='font-dmsans text-stone-700 text-sm uppercase tracking-widest'>{currArticle.source_id}</h5>
-                    <h3 className='font-dmsans font-bold text-neutral-headings-black text-xl'>{currArticle.title}</h3>
-                    <p className={`font-dmsans text-stone-700 text-base`}>{currArticle.description}</p>
+                    <h3 className='font-dmsans font-bold text-neutral-headings-black text-xl lg:line-clamp-5'>{currArticle.title}</h3>
+                    <p className={`font-dmsans text-stone-700 text-base line-clamp-5`}>{currArticle.description}</p>
                     <div className='h-5'></div>
                     <h5 className='font-dmsans text-stone-600 text-sm tracking-widest'>{currArticle.pubDate}</h5>
                 </div>
